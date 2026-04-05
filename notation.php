9730f29844199dd6a064c19512f4d32013321423
Radouane-prog <hadj.radouane1@gmail.com>
2026-04-05 15:50:44 +0200

    Feat: Added dynamic rating page

--- notation.php
+++ notation.php
@@ -2,6 +2,39 @@
     if (session_status() === PHP_SESSION_NONE) {
         session_start();
     }
+    date_default_timezone_set('Europe/Paris');
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $id_cmd = $_POST['id_commande'];
+        
+        $nouvel_avis = [
+            "id_commande" => $id_cmd,
+            "login_client" => $_SESSION['login'],
+            "note_livraison" => (int)$_POST['note_livraison'],
+            "note_qualite" => (int)$_POST['note_qualite'],
+            "commentaires" => $_POST['commentaires'],
+            "date_avis" => date('Y-m-d H:i:s')
+        ];
+
+        $chemin_avis = "data/avis.json";
+        $tous_les_avis = [];
+        if (file_exists($chemin_avis)) {
+            $contenu = file_get_contents($chemin_avis);
+            if (!empty($contenu)) $tous_les_avis = json_decode($contenu, true);
+        }
+
+        $tous_les_avis[] = $nouvel_avis;
+        file_put_contents($chemin_avis, json_encode($tous_les_avis, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+
+        header("Location: historique.php");
+        exit();
+    }
+
+    if (isset($_GET['id_commande'])) {
+        $id_commande_a_noter = $_GET['id_commande'];
+    } else {
+        die("<h2 style='color:#ff3333; text-align:center; padding-top:50px; font-family:\"Source Code Pro\", monospace;'>ERREUR CRITIQUE : MISSION NON IDENTIFIÉE.</h2>");
+    }
 ?>
 
 <!DOCTYPE html>
@@ -18,197 +51,34 @@
     <?php include "includes/header.php"; ?>
 
     <main>
-        <h1>FEEDBACK DE MISSION : NOTATION DE LA COMMANDE</h1>
-        <div id="container_avis">
+        <h1>FEEDBACK DE MISSION : <?= htmlspecialchars($id_commande_a_noter) ?></h1>
+        
+        <form action="notation.php" method="POST" id="container_avis">
+            <input type="hidden" name="id_commande" value="<?= htmlspecialchars($id_commande_a_noter) ?>">
 
             <h2>LIVRAISON (DÉPLOIEMENT)</h2>
-
-            <div class="container_stars">
-
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="32"
-                    height="32"
-                    class="star"
-                >
-                    <polygon
-                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        fill="none"
-                        stroke="#ff3333"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    />
-                </svg>
-
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="32"
-                    height="32"
-                    class="star"
-                >
-                    <polygon
-                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        fill="none"
-                        stroke="#ff3333"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    />
-                </svg>
-
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="32"
-                    height="32"
-                    class="star"
-                >
-                    <polygon
-                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        fill="none"
-                        stroke="#ff3333"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    />
-                </svg>
-
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="32"
-                    height="32"
-                    class="star"
-                >
-                    <polygon
-                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        fill="none"
-                        stroke="#ff3333"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    />
-                </svg>
-
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="32"
-                    height="32"
-                    class="star"
-                >
-                    <polygon
-                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        fill="none"
-                        stroke="#ff3333"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    />
-                </svg>
-
+            <div class="rating_css">
+                <input type="radio" name="note_livraison" id="liv5" value="5"><label for="liv5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></label>
+                <input type="radio" name="note_livraison" id="liv4" value="4"><label for="liv4"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></label>
+                <input type="radio" name="note_livraison" id="liv3" value="3"><label for="liv3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></label>
+                <input type="radio" name="note_livraison" id="liv2" value="2"><label for="liv2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></label>
+                <input type="radio" name="note_livraison" id="liv1" value="1" required><label for="liv1"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></label>
             </div>
 
             <h2>QUALITÉ DES PRODUITS</h2>
-
-            <div class="container_stars">
-
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="32"
-                    height="32"
-                    class="star"
-                >
-                    <polygon
-                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        fill="none"
-                        stroke="#ff3333"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    />
-                </svg>
-
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="32"
-                    height="32"
-                    class="star"
-                >
-                    <polygon
-                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        fill="none"
-                        stroke="#ff3333"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    />
-                </svg>
-
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="32"
-                    height="32"
-                    class="star"
-                >
-                    <polygon
-                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        fill="none"
-                        stroke="#ff3333"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    />
-                </svg>
-
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="32"
-                    height="32"
-                    class="star"
-                >
-                    <polygon
-                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        fill="none"
-                        stroke="#ff3333"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    />
-                </svg>
-
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="32"
-                    height="32"
-                    class="star"
-                >
-                    <polygon
-                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        fill="none"
-                        stroke="#ff3333"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    />
-                </svg>
-
+            <div class="rating_css">
+                <input type="radio" name="note_qualite" id="qual5" value="5"><label for="qual5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></label>
+                <input type="radio" name="note_qualite" id="qual4" value="4"><label for="qual4"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></label>
+                <input type="radio" name="note_qualite" id="qual3" value="3"><label for="qual3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></label>
+                <input type="radio" name="note_qualite" id="qual2" value="2"><label for="qual2"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></label>
+                <input type="radio" name="note_qualite" id="qual1" value="1" required><label for="qual1"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></label>
             </div>
 
             <h2>COMMENTAIRES (PATCH NOTES)</h2>
+            <textarea name="commentaires" id="commentaires" placeholder="> Entrez votre rapport ici..." required></textarea>
 
-            <textarea name="commentaires" id="commentaires" placeholder="> Entrez votre rapport ici..."></textarea>
-
-            <button id="submit_avis">[ ENVOYER LE FEEDBACK ]</button>
-        </div>        
+            <button type="submit" id="submit_avis">[ ENVOYER LE FEEDBACK ]</button>
+        </form>        
     </main>
 
     <footer>
